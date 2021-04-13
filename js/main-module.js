4f7316dd4b4269427d42594a83dce3a99038cfe2
federico <federico.mich97@gmail.com>
2021-04-13 11:52:32 +0200

    v1 progetto js

---
+++ js/main-module.js
@@ -0,0 +1,64 @@
+import {add} from './util/math.js'; // Importo un modulo esterno e in particolare una sua funzione.
+
+/**
+ * Funsione ad uso dimostrativo:
+ *  - La funzione è esportata dal modulo e utilizza al suo interno la funzione "add" precedentemente importata.
+ * 
+ * @param  {} a primo valore
+ * @param  {} b secondo valore
+ */
+export function callAdd (a, b) {
+  // Viene usata la funzione "parseInt()" su ogni parametro per convertire il valore in formato numerico.
+  // Si rende necessario questo cast perchè i value dei tag INPUT non sono tipizzati ma solo stringhe.
+  return add (parseInt (a), parseInt (b));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO: callAdd arrow function version
+
+export function addListElements () {
+  for (let index = 1; index < 5; index++) {
+    const node = document.createElement ('LI'); // Create a <li> node
+    const textnode = document.createTextNode ('Elemento' + index); // Create a text node
+    node.appendChild (textnode); // Append the text to <li>
+    document.getElementById ('myList').appendChild (node); // Append <li> to <ul> with id="myList"
+  } 
+}
+
+export function removeListElements() {
+    const domElement = document.getElementById('myList');
+    while (domElement.hasChildNodes()) {  
+        domElement.removeChild(domElement.firstChild);
+      }
+}
---
+++ js/util/math.js
@@ -0,0 +1,12 @@
+
+/**
+ * Semplice funzione che fa la somma dei due valori passati come parametri
+ *  - IMPORTANTE: notare la keyword iniziale sulla firma della funzione (export)
+ * @param  {} a primo valore 
+ * @param  {} b secondo valore
+ */
+export function add(a, b) {       
+    return a + b;
+}
+
+//TODO: Add arrow function version
